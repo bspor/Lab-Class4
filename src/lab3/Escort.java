d09bb40b255b9631dc31fae5ae835ef60ba46227
Brandon <Brandon@176.168.0.198>
2013-02-05 11:47:13 -0600

    added new class

--- src/lab3/Escort.java
+++ src/lab3/Escort.java
@@ -9,5 +9,20 @@ package lab3;
  * @author Brandon
  */
 public class Escort extends Car implements Cars {
+    //The Escort is a particular car. It is with this class that we will define
+    //all of the things that make an Escort and Escort and not a Mustang
+    
+   
+    public Escort(String color, int numWheels, String engineType) {
+        super(color, numWheels, engineType);
+    }
+
+    public String getHorn() {
+        return horn;
+    }
+
+    public void setHorn(String horn) {
+        this.horn = horn;
+    }
     
 }
